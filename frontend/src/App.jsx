ade957e9434f4b78f5e070d6a7a8abe42f140fba
varbees <harshavar968@gmail.com>
2023-07-17 19:56:53 +0530

    HomePage and Routes setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <Container className='my-2'>
+        <Outlet />
+      </Container>
+    </>
+  );
 };
 
 export default App;
---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,30 @@
+import { Container, Card, Button } from 'react-bootstrap';
+const Hero = () => {
+  return (
+    <div className='py-5'>
+      <Container className='d-flex justify-content-center'>
+        <Card className='p-5 d-flex flex-column align-items-center hero-card bg-body-secondary w-75'>
+          <h2 className='text-center mb-4'>
+            A Showcase On JWT Based Mern Authentication
+          </h2>
+          <p className='text-center mb-4'>
+            A classic boiler plate code configured with CRUD operations on users
+            with a backend API, Token is saved in a HTTP-only cookie. On the
+            Frontend it uses react, state management with redux, styling on
+            react bootstrap library
+          </p>
+          <div className='d-flex '>
+            <Button href='/login' variant='primary' className='me-3'>
+              Sign In
+            </Button>
+            <Button href='/register' variant='secondary' className='me-3'>
+              Sign Up
+            </Button>
+          </div>
+        </Card>
+      </Container>
+    </div>
+  );
+};
+
+export default Hero;
